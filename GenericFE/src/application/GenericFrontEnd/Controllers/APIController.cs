4e82c309a88d8f4b5458526c77e660cafa6bb07b
Yashar Aliabbasi <yashar@pragmateam.dev>
2020-10-10 16:15:27 +0330

    Getting ApplicationInfo from API an BE using REST/GET

---
+++ GenericFE/src/application/GenericFrontEnd/Controllers/APIController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Application;
+
+namespace GenericFrontEnd.Controllers
+{
+	public class APIController : Controller
+	{
+		[HttpGet]
+		public IActionResult ApplicationInfo()
+		{
+			var obj = new ApplicationInfo
+			{
+				InMaintenance = false,
+				Title = "ACCOUNTING",
+				Copyright = "copyright by phigaro 2020".ToUpper(),
+				Desc = "BY PHIGARO",
+				UserId = "Yashar",
+				UserInfo = "Software Engineer",
+				LogoImageURL = "/images/Tribit.png",
+				I18n = new i18n
+				{
+					Lang = 1065,
+					Translate = true
+				}
+			};
+			return Json(obj);
+		}
+	}
+}
---
+++ GenericFE/src/business/Models/Application/ApplicationInfo.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models;
+
+namespace Models.Application
+{
+	public class ApplicationInfo : Model
+	{
+		public bool InMaintenance { get; set; }
+		public string Title { get; set; }
+		public string Copyright { get; set; }
+		public string Desc { get; set; }
+		public string UserId { get; set; }
+		public string UserInfo { get; set; }
+		public string LogoImageURL { get; set; }
+		public i18n I18n { get; set; }
+	}
+}
